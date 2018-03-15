fea4d7e2f90f543609b555447487d00034a8674c
malabarMCB <grinevich97@gmail.com>
2018-03-15 23:02:40 +0200

    Remove redundant code from prev task

--- src/crossplatform/Program.java
+++ src/crossplatform/Program.java
@@ -44,50 +44,6 @@ public class Program extends Application {
 
     public static void main(String[] args){
         launch(args);
-       /*
-
-        //files
-        String logFilePath=csvFilePath.replace(".csv",".log");
-        String jsonFilePath=csvFilePath.replace(".csv",".json");
-
-        //dependencies
-        ILog consoleLogger= new ConsoleLogger();
-        ILog fileLogger= new FileLogger(logFilePath);
-        List<ILog> loggers= Arrays.asList(consoleLogger,fileLogger);
-        BookParserLoggable bookParser= new BookParserLoggable(loggers);
-
-        //program flow
-
-        List<String> booksStr= FileUtil.readAllLines(csvFilePath,1);
-        List<Book> books=new ArrayList<>();
-        for (String bookStr : booksStr) {
-            Book book=bookParser.Parse(bookStr);
-            if(book!=null)
-                books.add(book);
-        }
-
-        System.out.println("Collection:");
-        for (Book book : books) {
-            System.out.println(book);
-        }
-
-
-        List<IdentifiedBook> identifiedBooks= new ArrayList<>();
-        books.forEach(b->{
-            IdentifiedBook identifiedBook= new IdentifiedBook();
-            identifiedBook.udc=b.udc;
-            identifiedBook.author=b.author;
-            identifiedBook.name=b.name;
-            identifiedBook.publishYear=b.publishYear;
-            identifiedBook.instanceCount=b.instanceCount;
-
-            identifiedBooks.add(identifiedBook);
-        });
-
-        identifiedBooks.sort(Comparator.naturalOrder());
-
-        String json=JsonUtil.convertToJson(identifiedBooks);
-        FileUtil.writeLine(json,jsonFilePath);*/
     }
 }
 
