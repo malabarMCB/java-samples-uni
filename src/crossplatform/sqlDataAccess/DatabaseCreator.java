83a320b8a4c4ddbac64f1628439e1349366dc2b8
malabarMCB <grinevich97@gmail.com>
2018-04-05 21:58:10 +0300

    remove query from DatabaseCreator to stored procedure

--- src/crossplatform/sqlDataAccess/DatabaseCreator.java
+++ src/crossplatform/sqlDataAccess/DatabaseCreator.java
@@ -1,6 +1,6 @@
 package crossplatform.sqlDataAccess;
 
-import java.sql.Statement;
+import java.sql.PreparedStatement;
 
 public class DatabaseCreator {
     private String connectionString;
@@ -10,31 +10,11 @@ public class DatabaseCreator {
     }
 
     public void recreateDatabase(){
-        String query="IF OBJECT_ID('Book', 'U') IS NOT NULL\n" +
-                "DROP TABLE Book;\n" +
-                "IF OBJECT_ID('Logger', 'U') IS NOT NULL\n" +
-                "DROP TABLE Logger;\n" +
-                "\n" +
-                "create table Book\n" +
-                "(\n" +
-                "\tId int primary key,\n" +
-                "\tUDC nvarchar(255) not null unique,\n" +
-                "\tAuthor nvarchar(255) not null,\n" +
-                "\tName nvarchar(255) not null,\n" +
-                "\tPublishYear int,\n" +
-                "\tInstanceCount int default 0 \n" +
-                ")\n" +
-                "\n" +
-                "create table Logger\n" +
-                "(\n" +
-                "\tId int primary key identity(1,1),\n" +
-                "\tMessage nvarchar(255) not null,\n" +
-                "\tLogTime DateTime default getdate()\n" +
-                ")";
+        String query="{call RecreateTables}";
 
         SqlQueryExecutor.execute(connectionString,connnection -> {
-            Statement statement=connnection.createStatement();
-            statement.executeQuery(query);
+            PreparedStatement statement=connnection.prepareStatement(query);
+            statement.execute();
         });
     }
 }
